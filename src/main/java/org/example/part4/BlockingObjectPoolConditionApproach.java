6e8ffb07ac61becf0ceb0e855daceb818d767c4e
Rostyslav_Andrusiv <rostik_andrusiv@ukr.net>
2022-08-31 09:01:37 +0300

    refactor

--- src/main/java/org/example/part4/BlockingObjectPoolConditionApproach.java
+++ src/main/java/org/example/part4/BlockingObjectPoolConditionApproach.java
@@ -3,8 +3,8 @@ package org.example.part4;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -18,25 +18,21 @@ import java.util.concurrent.locks.ReentrantLock;
 @Slf4j
 public class BlockingObjectPoolConditionApproach {
 
-    private final Set<Object> pool = new HashSet<>();
+    private final Queue<Object> pool = new LinkedList<>();
 
-    final Lock lock = new ReentrantLock();
+    private final Lock lock = new ReentrantLock();
 
-    final Condition empty = lock.newCondition();
+    private final Condition empty = lock.newCondition();
 
-    final Condition full = lock.newCondition();
+    private final Condition full = lock.newCondition();
 
     private final int size;
-
-    private int availableObjects;
-
 
     /**
      * Creates filled pool of passed size * * @param size of pool
      */
     public BlockingObjectPoolConditionApproach(int size) {
         this.size = size;
-        availableObjects = size;
         init();
     }
 
@@ -49,43 +45,33 @@ public class BlockingObjectPoolConditionApproach {
     /**
      * Gets object from pool or blocks if pool is empty * * @return object from pool
      */
-    public Object get() {
+    public Object get() throws InterruptedException {
         lock.lock();
         try {
-            while (availableObjects == 0) {
+            while (pool.isEmpty()) {
                 log.info("Pool is empty");
                 empty.await();
             }
-            var instance = pool.iterator().next();
-            pool.remove(instance);
-            availableObjects--;
-            full.signalAll();
+            var instance = pool.poll();
+            full.signal();
             return instance;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            Thread.currentThread().interrupt();
         } finally {
             lock.unlock();
         }
-        return null;
     }
 
     /**
      * Puts object to pool or blocks if pool is full * * @param object to be taken back to pool
      */
-    public void take(Object object) {
+    public void take(Object object) throws InterruptedException {
         lock.lock();
         try {
-            while (availableObjects == size) {
+            while (pool.size() == size) {
                 log.info("Pool is full");
                 full.await();
             }
-            pool.add(object);
-            availableObjects++;
-            empty.signalAll();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            Thread.currentThread().interrupt();
+            pool.offer(object);
+            empty.signal();
         } finally {
             lock.unlock();
         }
--- src/main/java/org/example/part4/BlockingObjectPoolSemaphore.java
+++ src/main/java/org/example/part4/BlockingObjectPoolSemaphore.java
@@ -3,8 +3,8 @@ package org.example.part4;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.Semaphore;
 
 
@@ -16,60 +16,39 @@ import java.util.concurrent.Semaphore;
 @Slf4j
 public class BlockingObjectPoolSemaphore {
 
-    private final Set<Object> pool = new HashSet<>();
+    private final Queue<Object> pool;
 
-    Semaphore semaphore;
+    private Semaphore semaphoreFull;
+
+    private Semaphore semaphoreEmpty;
 
     protected final int size;
-
-    private int availableObjects;
 
     /**
      * Creates filled pool of passed size * * @param size of pool
      */
     public BlockingObjectPoolSemaphore(int size) {
         this.size = size;
-        availableObjects = size;
-        init();
-    }
-
-    private void init() {
-        for (int i = 0; i < size; i++) {
-            pool.add(new Object());
-        }
-        semaphore = new Semaphore(size);
+        pool = new LinkedBlockingQueue<>(size);
+        semaphoreFull = new Semaphore(size);
+        semaphoreEmpty = new Semaphore(size);
     }
 
     /**
      * Gets object from pool or blocks if pool is empty * * @return object from pool
      */
-    public Object get() {
-        try {
-            if(availableObjects==0){
-                log.info("Pool is empty");
-                return null;
-            }
-            semaphore.acquire();
-            var instance = pool.iterator().next();
-            pool.remove(instance);
-            return instance;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            Thread.currentThread().interrupt();
-        }
-        return null;
+    public Object get() throws InterruptedException {
+        semaphoreFull.acquire();
+        semaphoreEmpty.release();
+        return pool.poll();
     }
 
     /**
      * Puts object to pool or blocks if pool is full * * @param object to be taken back to pool
      */
-    public void take(Object object) {
-        if(size>=availableObjects){
-            log.info("Pool is full");
-            return;
-        }
-
-        semaphore.release();
+    public void take(Object object) throws InterruptedException {
+        semaphoreEmpty.acquire();
+        semaphoreFull.release();
         pool.add(object);
     }
 }
